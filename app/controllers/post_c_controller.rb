fc1f16a6b8ad82092d87f68057ac5356284c60af
minisera <koki.ohtaka0821@gmail.com>
2020-10-26 11:33:14 +0900

    PostCコントローラー更新

--- app/controllers/post_c_controller.rb
+++ app/controllers/post_c_controller.rb
@@ -1,5 +1,29 @@
 class PostCController < ApplicationController
   def new
+    @post_c = PostC.new
+  end
+
+  def create
+    @post_c = PostC.new(postc_params)
+    if @post_c.save
+      redirect_to user_url(current_user.id)
+    else
+      render :nwe
+    end
+  end
+
+  def index
     
   end
+
+  def show
+    
+  end
+
+private
+
+  def postc_params
+    params.require(:post_c).permit(:title,:image,:place,:brand,:story,:evidence)
+  end
+
 end
