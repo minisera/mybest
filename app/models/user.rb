9419b1bf4e2cd102c03147f70a8fb5595c40b097
minisera <koki.ohtaka0821@gmail.com>
2020-10-27 11:24:09 +0900

    ユーザー登録　デフォルト画像追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
+  before_create :default_image
+
+  def default_image
+    if !self.profile_image.attached?
+      self.profile_image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'user_default.png')), filename: 'default-image.png', content_type: 'image/png')
+    end
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_image
   has_many :post_cs
+
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
