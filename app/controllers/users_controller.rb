4cc980143b48dc657eda4e95203c7f38b9d9d3c0
minisera <koki.ohtaka0821@gmail.com>
2020-10-28 15:53:05 +0900

    プロフィール設定ビュー追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.save
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
 end
