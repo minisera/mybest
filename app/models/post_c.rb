d08b032f57b6b04bb59a1d351543292d52ef806a
minisera <koki.ohtaka0821@gmail.com>
2020-10-29 17:12:55 +0900

    投稿ページフロントエンド改善

--- app/models/post_c.rb
+++ app/models/post_c.rb
@@ -2,4 +2,12 @@ class PostC < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options  presence: true do
+    validates :image
+    validates :title
+    validates :place
+    validates :brand
+    validates :story
+    validates :evidence
+  end
 end
