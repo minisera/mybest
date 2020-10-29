6b37e19d471e5524cd6d50b9029f646b881b9c24
minisera <koki.ohtaka0821@gmail.com>
2020-10-29 19:00:08 +0900

    服特録数制限追加

--- app/models/post_c.rb
+++ app/models/post_c.rb
@@ -10,4 +10,12 @@ class PostC < ApplicationRecord
     validates :story
     validates :evidence
   end
+
+  validate :limit_post_c,on: :create
+
+  def limit_post_c
+    if user && user.post_cs.size >= 10
+      errors.add(:user, "登録の制限数を超えました")
+    end
+  end
 end
