db1eacd763c237bab4352cd48c30fd40610c2e32
minisera <koki.ohtaka0821@gmail.com>
2020-11-16 06:17:35 +0900

    unicorn変更

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    # config.autoloader = :classic
     # 日本語の言語設定
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
