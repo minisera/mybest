48e83dca960e4e5885c12a0e605831d70780ee52
minisera <koki.ohtaka0821@gmail.com>
2020-12-03 14:42:24 +0900

    衣類投稿へのコメントajax化

--- app/controllers/comment_cs_controller.rb
+++ app/controllers/comment_cs_controller.rb
@@ -1,28 +1,32 @@
 class CommentCsController < ApplicationController
+  before_action :set_comment, only: :create
+
   def create
     @comment = CommentC.new(comment_params)
-    @comment.save
-    redirect_back(fallback_location: root_path)
-    
-    #   respond_to do | format |
-    #     format.json
-    #   end
-    # else
-    #   # render post_c_path(@comment.post_c.id)
-    # # flash[:notice] = '投稿にコメントしました'
-    # end
+    respond_to do |format|
+      if @comment.save
+        format.js
+        flash[:notice] = 'コメントを投稿しました'
+      end
+    end
   end
-
+  
   def destroy
     comment = CommentC.find(params[:post_c_id])
     comment.destroy
     redirect_back(fallback_location: root_path)
     flash[:notice] = 'コメントを削除しました'
   end
-
+  
   private
-
+  
   def comment_params
     params.require(:comment_c).permit(:text).merge(user_id: current_user.id, post_c_id: params[:post_c_id])
   end
+  
+  def set_comment
+    @post = PostC.find(params[:post_c_id])
+    @comments = @post.comment_cs.includes(:user)
+  end
+    
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,8 @@ require("../pulldown")
 require("../flash")
 require("../comment")
 
+window.$ = jQuery;
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
