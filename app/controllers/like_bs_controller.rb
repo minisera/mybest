4fc3a0cfd8ca4d7778a881c7496c806f460b762a
minisera <koki.ohtaka0821@gmail.com>
2020-12-04 16:16:22 +0900

    全投稿いいね create,destroy ajax化

--- app/controllers/like_bs_controller.rb
+++ app/controllers/like_bs_controller.rb
@@ -1,21 +1,21 @@
 class LikeBsController < ApplicationController
   def create
-    @like = current_user.like_bs.build(post_b_id: params[:post_b_id])
+    like = current_user.like_bs.build(post_b_id: params[:post_b_id])
     respond_to do |format|
-      if @like.save
+      if like.save
         @post = PostB.find(params[:post_b_id])
         format.js
       end
     end
-
-    # redirect_back(fallback_location: root_path)
-    # flash[:notice] = '投稿に「いいね」しました'
   end
 
   def destroy
     like = LikeB.find_by(post_b_id: params[:post_b_id], user_id: current_user.id)
-    like.destroy
-    redirect_back(fallback_location: root_path)
-    flash[:notice] = '投稿から「いいね」を外しました'
+    respond_to do |format|
+      if like.destroy
+        @post = PostB.find(params[:post_b_id])
+        format.js
+      end
+    end
   end
 end
