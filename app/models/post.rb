7ba6513e6e9ae68c5df9fb7bcce7fdde2c5fcfe0
minisera <koki.ohtaka0821@gmail.com>
2020-12-20 17:37:31 +0900

    docs: PostテーブルSTI化のためリレーション変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,31 @@
 class Post < ApplicationRecord
+  acts_as_taggable_on :tags
+  belongs_to :user
+  has_one_attached :image
+  has_many :likes, dependent: :destroy
+  has_many :picks, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  with_options presence: true do
+    validates :image
+    validates :title, length: { maximum: 12 }
+    validates :place
+    validates :brand
+    validates :story, length: { in: 30..200 }
+    validates :evidence, length: { in: 30..200 }
+  end
+
+  validate :limit_post, on: :create
+
+  def limit_post
+    errors.add(:user, 'の登録制限数を超えました') if user && user.posts.size > 9
+  end
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
+
+  def picked_by?(user)
+    picks.where(user_id: user.id).exists?
+  end
 end
