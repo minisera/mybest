34a8e7688c765c075ea0ce178933e43ed27f2ff4
minisera <koki.ohtaka0821@gmail.com>
2020-12-29 14:58:29 +0900

    feat: トレンド表示のため、postコントローラの記述変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,25 +12,14 @@ class PostsController < ApplicationController
       @posts = Post.where(type: const_name).includes(:user).page(params[:page]).per(18)
     else
       posts = Post.includes(:user)
-      @posts_b = posts.select { |x| x[:type].include?('Book') }
-      @posts_c = posts.select { |x| x[:type].include?('Clothe') }
-      @posts_g = posts.select { |x| x[:type].include?('Good') }
+      sort_post_type(posts)
     end
   end
-
-  def trend_index
-    @post_c_ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(9).pluck(:post_id)).select { |x| x[:type].include?('Clothe') }
-    @post_b_ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(9).pluck(:post_id)).select { |x| x[:type].include?('Book') }
-    @post_g_ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(9).pluck(:post_id)).select { |x| x[:type].include?('Good') }
-  end
-
-  def about
-  end
-
+  
   def new
     @post = @post_class.new
   end
-
+  
   def create
     @post = @post_class.new(post_params)
     if @post.save
@@ -39,15 +28,15 @@ class PostsController < ApplicationController
       render :new
     end
   end
-
+  
   def show
     @comment = Comment.new
     @comments = @post.comments.includes(:user)
   end
-
+  
   def edit
   end
-
+  
   def update
     if @post.update(post_params)
       redirect_to user_path(current_user)
@@ -55,46 +44,61 @@ class PostsController < ApplicationController
       render :edit
     end
   end
-
+  
   def destroy
     @post.destroy
     redirect_to user_path(current_user)
   end
-
+  
   def tag_index
     @posts = Post.tagged_with(params[:tag])
     @tag = params[:tag]
   end
-
-  private
-
+  
+  def trend_index
+    posts = Post.includes(:user).sort{|a,b| b.likes.size <=> a.likes.size}
+    sort_post_type(posts)
+  end
+  
+  def about
+  end
+  
+private
+  
   def use_before_action?
     false
   end
-
+  
+  def set_new_post
+    const_name = @post_name.gsub(/\b\w/) { |s| s.upcase }
+    @post_class = self.class.const_get(const_name)
+  end
+  
   def post_params
     params.require(@post_name).permit(:title, :image, :place, :brand, :story, :evidence, :tag_list).merge(user_id: current_user.id)
   end
-
+  
   def move_to_index
     unless current_user == @post.user
       redirect_to posts_url
       flash[:alert] = '他人の投稿は編集できません'
     end
   end
-
+  
   def set_post
     @post = Post.find(params[:id])
   end
-
-  def set_new_post
-    const_name = @post_name.gsub(/\b\w/) { |s| s.upcase }
-    @post_class = self.class.const_get(const_name)
-  end
-
+  
   def set_tag
-    @tag_cs = Clothe.tag_counts_on(:tags).most_used(10)
-    @tag_bs = Book.tag_counts_on(:tags).most_used(10)
-    @tag_gs = Good.tag_counts_on(:tags).most_used(10)
+    @tags_c = Clothe.tag_counts_on(:tags).most_used(10)
+    @tags_b = Book.tag_counts_on(:tags).most_used(10)
+    @tags_g = Good.tag_counts_on(:tags).most_used(10)
   end
+  
+  def sort_post_type(posts)
+    @posts_b = posts.select { |x| x[:type].include?('Book') }.take(9)
+    @posts_c = posts.select { |x| x[:type].include?('Clothe') }.take(9)
+    @posts_g = posts.select { |x| x[:type].include?('Good') }.take(9)
+  end
+
 end
