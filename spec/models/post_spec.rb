c452e4b089e493dfaa9d26f6ee8fe72462588749
minisera <koki.ohtaka0821@gmail.com>
2020-12-29 15:36:12 +0900

    feat: 新しいテーブルにて、エラー文を日本語になるよう修正

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   before do
-    @post = FactoryBot.build(:post_c)
+    @post = FactoryBot.build(:post)
   end
 
   describe '新規投稿機能、投稿編集機能' do
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,98 +2,98 @@ require 'rails_helper'
 
 RSpec.describe 'PoctCsController', type: :request do
   before do
-    @post = FactoryBot.create(:post_c)
+    @post = FactoryBot.create(:post)
   end
 
   describe 'GET #show' do
     it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
-      get post_c_path(@post)
+      get post_path(@post)
       expect(response.status).to eq 200
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みのタイトルが存在する' do
-      get post_c_path(@post)
+      get post_path(@post)
       expect(response.body).to include @post.title
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みのブランドが存在する' do
-      get post_c_path(@post)
+      get post_path(@post)
       expect(response.body).to include @post.brand
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みの出会った場所が存在する' do
-      get post_c_path(@post)
+      get post_path(@post)
       expect(response.body).to include @post.place
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みの出会うまでのストーリーが存在する' do
-      get post_c_path(@post)
+      get post_path(@post)
       expect(response.body).to include @post.story
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みのお気に入りポイントが存在する' do
-      get post_c_path(@post)
+      get post_path(@post)
       expect(response.body).to include @post.evidence
     end
     it 'showアクションにリクエストするとレスポンスに画像が存在する' do
-      get post_c_path(@post)
+      get post_path(@post)
       expect(response.body).to include 'test_image.png'
     end
     it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do
-      get post_c_path(@post)
+      get post_path(@post)
       expect(response.body).to include 'コメント一覧'
     end
   end
   describe 'GET #index' do
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
-      get post_cs_path
+      get posts_path
       expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのタイトルが存在する' do
-      get post_cs_path
+      get posts_path
       expect(response.body).to include @post.title
     end
     it 'indexアクションにリクエストするとレスポンスに画像が存在する' do
-      get post_cs_path
+      get posts_path
       expect(response.body).to include 'test_image.png'
     end
     it 'indexアクションにリクエストすると投稿者のアバターが存在する' do
-      get post_cs_path
+      get posts_path
       expect(response.body).to include 'test_user.png'
     end
     it 'indexアクションにリクエストすると投稿者の名前が存在する' do
-      get post_cs_path
+      get posts_path
       expect(response.body).to include @post.user.name
     end
   end
 
   describe 'GET #index' do
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
-      get post_cs_path
+      get posts_path
       expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのタイトルが存在する' do
-      get post_cs_path
+      get posts_path
       expect(response.body).to include @post.title
     end
     it 'indexアクションにリクエストするとレスポンスに画像が存在する' do
-      get post_cs_path
+      get posts_path
       expect(response.body).to include 'test_image.png'
     end
     it 'indexアクションにリクエストすると投稿者のアバターが存在する' do
-      get post_cs_path
+      get posts_path
       expect(response.body).to include 'test_user.png'
     end
     it 'indexアクションにリクエストすると投稿者の名前が存在する' do
-      get post_cs_path
+      get posts_path
       expect(response.body).to include @post.user.name
     end
   end
 
   describe 'GET #edit' do
     it 'editアクションにリクエストすると正常にレスポンスが返ってくる' do
-      get post_cs_path(@post)
+      get posts_path(@post)
       expect(response.status).to eq 200
     end
   end
   describe 'PUT #update' do
     it 'updateアクションにリクエストすると正常にレスポンスが返ってくる' do
-      put post_c_path(@post), params: { title: '新しいタイトル' }
+      put post_path(@post), params: { title: '新しいタイトル' }
       expect(@post.reload.title).to eq '新しいタイトル'
     end
   end
